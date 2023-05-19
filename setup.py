ae781b0d4b71220443dc8d89139ed550e4e4ef5e
edjones84 <ejones84@icloud.com>
2023-05-19 17:18:02 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
