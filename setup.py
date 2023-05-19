2fd0d058d098bc728ecd21d90813498fdac15ae7
edjones84 <ejones84@icloud.com>
2023-05-19 16:03:36 +0100

    add the manifest file

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hyper-requests",
+    version="0.0.1",
+    author="Edward Jones",
+    author_email="ejones84@icloud.com",
+    description="Concurrent request HTTP execution library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="package URL",
+    project_urls={
+        "Bug Tracker": "package issues URL",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
